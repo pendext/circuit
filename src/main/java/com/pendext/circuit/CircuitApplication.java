7684b9491d47b903318223f3006230e623876b47
john p <john.pendexter@gmail.com>
2018-01-09 11:38:10 -0500

    Enabling the dashboard

--- src/main/java/com/pendext/circuit/CircuitApplication.java
+++ src/main/java/com/pendext/circuit/CircuitApplication.java
@@ -2,8 +2,13 @@ package com.pendext.circuit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class CircuitApplication {
 
 	public static void main(String[] args) {
